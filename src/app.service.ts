529738ed0f6df12e761adf6a0c9059ef8085513f
Jonathan Tovar Diaz <jonitiso@gmail.com>
2024-03-13 22:42:10 -0500

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,28 +8,61 @@ import { exec } from 'child_process';
 export class AppService {
   constructor(@Inject('ECOM_SAT_SERVICE') private client: ClientProxy) {}
 
-  async asyncRun(filename: string) {
+  async runCmnd(cmd: string) {
     return new Promise((resolve, reject) => {
-      exec(`python /home/pi/ecom/ecom/${filename}`, (error, stdout, stderr) => {
-        if (error) {
-          reject(error);
-        }
-        if (stderr) {
-          reject(stderr);
+      exec(cmd, (error, stdout, stderr) => {
+        if (error || stderr) {
+          reject({
+            error,
+            response: stderr,
+          });
         }
         resolve(stdout);
       });
     });
   }
 
-  async interact(data: { filename: string }) {
+  async runPython(filename: string) {
+    try {
+      return await this.runCmnd(`python /home/pi/ecom/ecom/${filename}`);
+    } catch (e: any) {
+      console.error('Error running python: ', e);
+    }
+  }
+
+  async interact(data: { filename: string; interactionId: string }) {
     console.log('Interacting with: ', data);
     try {
-      const response = await this.asyncRun(data.filename);
+      const response = await this.runCmnd('whoami');
       console.log('Response: ', response);
-    } catch (e) {
+      await this.emitInteractResponse({
+        success: true,
+        response: response,
+        interactionId: data.interactionId,
+      });
+    } catch (e: any) {
       console.error('Error interacting: ', e);
+      await this.emitInteractResponse({
+        success: false,
+        error: {
+          trace: e.error,
+          message: e.response,
+        },
+        interactionId: data.interactionId,
+      });
     }
+  }
+
+  async emitInteractResponse(data: {
+    success: boolean;
+    response?: any;
+    error?: {
+      message: string;
+      trace: any;
+    };
+    interactionId: string;
+  }) {
+    this.client.emit('ecom/0a-00-27-00-00-0a/interact-response', data);
   }
 
   async getMacAddress() {
